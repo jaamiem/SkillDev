8a0fc29e59ad91fe3c03ebb426f0937ddefdf5e9
Jim Muir <jamiemuir1994@gmail.com>
2018-03-22 17:55:47 +0000

    work on routing

--- WebDev/FindSpot/app.js
+++ WebDev/FindSpot/app.js
@@ -4,6 +4,19 @@ var ejs = require('ejs');
 
 var app = express();
 
+var spots = [
+	{
+		name: "College Street Car Park",
+		lat: 57.142736,
+		lng: -2.099031
+	},
+	{
+		name: "Crown Terrace",
+		lat:57.144050,
+		lng:-2.101225
+	}
+]
+
 //  Set the view engine to read EJS files for templating
 app.set('view engine', 'ejs');
 
@@ -11,13 +24,15 @@ app.use(express.static(__dirname + '/public'));
 
 //  Initial page
 app.get('/', (req, res) => {
-	res.render('home');
+	res.render('home', { spots: spots });
 });
 
+//  About Page
 app.get('/about', (req,res) => {
 	res.render('about');
 })
 
+//  Help Page
 app.get('/help', (req,res) => {
 	res.render('help');
 })
